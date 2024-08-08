27cad52191c59bec85734ac820807cf489b20821
Nikolai <n_prusov@inbox.ru>
2024-08-08 22:10:36 +0300

    add: not work function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+/* module.exports =  */
+
+function towelSort (matrix) {
+  for (let i = 0; i <= matrix.length; i++) {
+    for (let j = 0; j <= matrix[i].length; j++) {
+      console.log (matrix[i][j]);
+    }
+  }
+
+
+  return matrix;
 }
+
+
+let matr = [
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+ ];
+
+
+let z = towelSort (matr);
+
+

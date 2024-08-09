27c67f0cd376f978d8587a98d6f0fb838f1fa565
Invisi6le45 <n_prusov@inbox.ru>
2024-08-09 13:28:26 +0300

    fix: problem empy array

--- src/index.js
+++ src/index.js
@@ -2,50 +2,33 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  let result = [];
-  console.log (matrix.length);
 
-  if (matrix.length === 0 || matrix === 0){
+
+  let result = [];
+  
+  if (!matrix){
     return result;
   } else {
     for (let i = 0; i < matrix.length; i++) {
-      
-      
       if ( i === 0 ) {
         for (let j = 0; j < matrix[i].length; j++) {
           result.push(matrix[i][j])
         }
-        console.log ('Массив result: ' + result);
       } else {
-
         if ( i % 2 !== 0) {
           for (let j = (matrix[i].length-1) ; j >= 0; j--) {
             result.push(matrix[i][j])
           }  
-          console.log ('Массив result: ' + result);
         } else {
           for (let j = 0; j < matrix[i].length; j++) {
             result.push(matrix[i][j])
-            console.log ('Массив result: ' + result);
           }
         }
       }
-
-
-
-
-
-
-
-
-
-
-
-      
     }
   }
   
-  console.log (matrix);
-  console.log (result);  
+
+  console.log ('Одномерный выходной массив имеет вид: ' + result);  
   return result;
 }

e9978e1b67b3cebcbc1f8b6026773c4fde781200
Invisi6le45 <n_prusov@inbox.ru>
2024-08-09 10:37:03 +0300

    add function 1 error test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,50 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  console.log (matrix.length);
+
+  if (matrix.length === 0 || matrix === 0){
+    return result;
+  } else {
+    for (let i = 0; i < matrix.length; i++) {
+      
+      
+      if ( i === 0 ) {
+        for (let j = 0; j < matrix[i].length; j++) {
+          result.push(matrix[i][j])
+        }
+        console.log ('Массив result: ' + result);
+      } else {
+
+        if ( i % 2 !== 0) {
+          for (let j = (matrix[i].length-1) ; j >= 0; j--) {
+            result.push(matrix[i][j])
+          }  
+          console.log ('Массив result: ' + result);
+        } else {
+          for (let j = 0; j < matrix[i].length; j++) {
+            result.push(matrix[i][j])
+            console.log ('Массив result: ' + result);
+          }
+        }
+      }
+
+
+
+
+
+
+
+
+
+
+
+      
+    }
+  }
+  
+  console.log (matrix);
+  console.log (result);  
+  return result;
 }
